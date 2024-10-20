7769acb5adad14dd4e25459353d6c52739318b35
Vlad Brinzas <vlad.brinzas@gmail.com>
2024-10-21 00:52:07 +0300

    Tidied up cellElementToCellObject.js
    Switched from using large, bulky switch() statements to having two
    hard-coded Map objects and retrieving from those.
    
    The issue about needing to convert String objects to native strings
    remains, however, since map get() also runs into that issue.

--- cellElementToCellObject.js
+++ cellElementToCellObject.js
@@ -1,83 +1,45 @@
 const cellElementToCellObject = function(cellDOMElement) {
-    // Get element classes that define type and rotation
-    const currentGridPipeClass = cellDOMElement.className.match(/pipe[1-9][0-4]?/); // 'pipe1' to 'pipe14'
-    const currentGridRotationClass = cellDOMElement.className.match(/cell-[0-3]/); // 'cell-0' to 'cell-3'
+    // Define class-to-value equivalence maps
+    // These are hard-coded from observation; if puzzle-pipes.com makes changes,
+    // they may need to be re-researched and updated
+    const rotationClassToNumberOfRotations = new Map([
+        ["cell-0", 0],
+        ["cell-3", 1],
+        ["cell-2", 2],
+        ["cell-1", 3]
+    ]);
+    const pipeClassToCellClassAndStartingRotations = new Map([
+        ["pipe1",  { cls: NubCell,      rot: 3 }],
+        ["pipe2",  { cls: NubCell,      rot: 2 }],
+        ["pipe3",  { cls: ElbowCell,    rot: 0 }],
+        ["pipe4",  { cls: NubCell,      rot: 1 }],
+        ["pipe5",  { cls: LineCell,     rot: 1 }],
+        ["pipe6",  { cls: ElbowCell,    rot: 3 }],
+        ["pipe7",  { cls: ThreeWayCell, rot: 2 }],
+        ["pipe8",  { cls: NubCell,      rot: 0 }],
+        ["pipe9",  { cls: ElbowCell,    rot: 1 }],
+        ["pipe10", { cls: LineCell,     rot: 0 }],
+        ["pipe11", { cls: ThreeWayCell, rot: 3 }],
+        ["pipe12", { cls: ElbowCell,    rot: 2 }],
+        ["pipe13", { cls: ThreeWayCell, rot: 0 }],
+        ["pipe14", { cls: ThreeWayCell, rot: 1 }]
+    ]);
 
-    // Hacky lambda to enclose a switch in an assignment
-    const clockwiseRotationsFromRotationClass = ((currentGridRotationClass) => {
-        // Hack: turn back to primitive JavaScript string (as opposed to
-        // String *object*, so switch calls can work properly)
-        switch (String(currentGridRotationClass)) {
-            case "cell-0": return 0;
-            case "cell-3": return 1;
-            case "cell-2": return 2;
-            case "cell-1": return 3;
-        }
-    })(currentGridRotationClass);
+    // Get element CSS classes that identify type and rotation
 
-    // Hacky lambda AND ugly switch; maybe we'd want a hard-coded map or something?
-    // TODO: consider alternatives to a hard-coded switch
-    const {CellClassToCreate, clockwiseRotationsFromPipeClass} = ((currentGridPipeClass) => {
-        let CellClassToCreate, clockwiseRotationsFromPipeClass;
-        
-        // Hack: turn back to primitive JavaScript string (as opposed to
-        // String *object*, so switch calls can work properly)
-        currentGridPipeClass = String(currentGridPipeClass);
+    // Hack: these return JavaScript String objects, which do not match with JavaScript
+    // native string literals if === is used instead of ==. Both maps and switch()
+    // statements - the solutions I tried - do a === check, however.
+    // So, to hack our way around that, we turn these back into native strings.
+    // (That's what the String(...) does.)
+    const currentGridPipeClass = String(cellDOMElement.className.match(/pipe[1-9][0-4]?/)); // 'pipe1' to 'pipe14'
+    const currentGridRotationClass = String(cellDOMElement.className.match(/cell-[0-3]/)); // 'cell-0' to 'cell-3'
 
-        // Figure what type of cell this is
-        switch (currentGridPipeClass) {
-            case "pipe1":
-            case "pipe2":
-            case "pipe4":
-            case "pipe8":
-                CellClassToCreate = NubCell;
-                break;
-            case "pipe3":
-            case "pipe6":
-            case "pipe9":
-            case "pipe12":
-                CellClassToCreate = ElbowCell;
-                break;
-            case "pipe5":
-            case "pipe10":
-                CellClassToCreate = LineCell;
-                break;
-            case "pipe7":
-            case "pipe11":
-            case "pipe13":
-            case "pipe14":
-                CellClassToCreate = ThreeWayCell;
-                break;
-        }
-
-        // Figure how many times it rotated from the "default" orientation for that type
-        switch (currentGridPipeClass) {
-            case "pipe3":
-            case "pipe8":
-            case "pipe10":
-            case "pipe13":
-                clockwiseRotationsFromPipeClass = 0;
-                break;
-            case "pipe4":
-            case "pipe5":
-            case "pipe9":
-            case "pipe14":
-                clockwiseRotationsFromPipeClass = 1;
-                break;
-            case "pipe2":
-            case "pipe7":
-            case "pipe12":
-                clockwiseRotationsFromPipeClass = 2;
-                break;
-            case "pipe1":
-            case "pipe6":
-            case "pipe11":
-                clockwiseRotationsFromPipeClass = 3;
-                break;
-        }
-
-        return {CellClassToCreate, clockwiseRotationsFromPipeClass};
-    })(currentGridPipeClass);
+    // Figure out what type of cell it needs to be
+    // and fetch rotation data to compute facing direction later
+    const clockwiseRotationsFromRotationClass = rotationClassToNumberOfRotations.get(currentGridRotationClass);
+    const { cls: CellClassToCreate, rot: clockwiseRotationsFromPipeClass }
+        = pipeClassToCellClassAndStartingRotations.get(currentGridPipeClass);
 
     // Figure out cell facing direction (default in our system is DOWN)
     const totalRotations = clockwiseRotationsFromRotationClass + clockwiseRotationsFromPipeClass;
