080312dc0161382598c5b96f040d34c3dfc0510a
Vlad Brinzas <vlad.brinzas@gmail.com>
2024-10-21 16:33:17 +0300

    Added a little menu to puzzle-pipes.com page for our functionality
    Added menuInjector.js, which generates HTML elements and injects them
    into the puzzle-pipes.com page. It also covers night mode compliance (so
    if the user has night mode on or toggles it on/off, our elements adjust).
    
    Changed stringifyGrid function to allow custom newline delimiters, so
    it's easy to use HTML <br> tags instead where needed.
    
    Current menu functionality is sparse, but more can quickly be added now
    that the menu itself exists.

---
+++ menuInjector.js
@@ -0,0 +1,69 @@
+// -------------------------------------------------------------------------------------------
+// | Step 1: Locate the DOM element storing the puzzle buttons (like Done, Start Over, etc.) |
+// -------------------------------------------------------------------------------------------
+const buttonsPanelDOMElement = document.querySelector(".puzzleButtons");
+
+// ------------------------------------------
+// | Step 1.5: Set up night mode compliance |
+// ------------------------------------------
+const nightModeCheckBox = document.querySelector("#night-mode");
+const nightModeCompliantElements = [];
+let isNightModeOn = nightModeCheckBox.checked;
+const NIGHTMODE_CLASS_NAME = "nightmode";
+nightModeCheckBox.addEventListener("change", () => {
+    isNightModeOn = nightModeCheckBox.checked;
+    for (const element of nightModeCompliantElements) {
+        element.classList.toggle(NIGHTMODE_CLASS_NAME);
+    }
+});
+
+// ----------------------------------------
+// | Step 2: Produce our own little panel |
+// ----------------------------------------
+// Helper function to nicely tag all the elements we add
+const createPipesSolverElement = function(...args) {
+    const element = document.createElement(...args);
+    element.className = "pipesSolver";
+
+    // Night mode compliance
+    if (isNightModeOn) element.classList.add("nightmode");
+    nightModeCompliantElements.push(element);
+
+    return element;
+};
+
+// Indentation used to show which thing goes where, but it'd probably be better to use
+// files or functions or some other form of separation
+const solverMenuPanel = createPipesSolverElement("div");
+solverMenuPanel.id = "solverMenuPanel";
+    const showGridSubPanel = createPipesSolverElement("div");
+    showGridSubPanel.id = "showGridSubPanel";
+        const gridDisplayDiv = createPipesSolverElement("div");
+        gridDisplayDiv.id = "gridDisplay";
+
+        const refreshGridDisplayButton = createPipesSolverElement("button");
+        refreshGridDisplayButton.classList.add("button");
+        refreshGridDisplayButton.id = "refreshGridDisplayButton"
+        refreshGridDisplayButton.innerText = "Refresh Grid Display"
+        refreshGridDisplayButton.onclick = (clickEvent) => {
+            // Without preventDefault(), puzzle-pipes.com refreshes the page sort-of,
+            // which erases our nicely-added ASCII display
+            clickEvent.preventDefault();
+
+            // Kinda' hacky that we use these values here without saying
+            // that we're borrowing them from solver.js
+            gridDisplayDiv.innerHTML = stringifyGrid(grid, GRID_HEIGHT, GRID_WIDTH, "<br>");
+        }
+
+        showGridSubPanel.appendChild(refreshGridDisplayButton);
+        showGridSubPanel.appendChild(gridDisplayDiv);
+    solverMenuPanel.appendChild(showGridSubPanel);
+
+// ---------------------------------------------------------------
+// | Step 3: Insert our own buttons panel below the current ones |
+// ---------------------------------------------------------------
+// We attach after the share container, but the <p> below it with the YouTube
+// tutorial could also be a decent choice, or we could just appendChild() and sit
+// at the certified end
+const sharePanelDOMElement = buttonsPanelDOMElement.querySelector("#shareContainer");
+buttonsPanelDOMElement.insertBefore(solverMenuPanel, sharePanelDOMElement.nextSibling);
--- stringifyGrid.js
+++ stringifyGrid.js
@@ -1,11 +1,11 @@
-const stringifyGrid = function(grid, gridHeight, gridWidth) {
+const stringifyGrid = function(grid, gridHeight, gridWidth, newlineDelimiter = '\n') {
     let stringifiedGrid = "";
 
     for (let colIndex = 0; colIndex < gridHeight; ++colIndex) {
         for (let rowIndex = 0; rowIndex < gridWidth; ++rowIndex) {
             stringifiedGrid += grid[colIndex][rowIndex];
         }
-        stringifiedGrid += '\n';
+        stringifiedGrid += newlineDelimiter;
     }
 
     return stringifiedGrid;
